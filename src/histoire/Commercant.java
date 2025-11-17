55839b645e831f48c917dca3e7cef39832e3d4f5
Mederic Mognat <mgm5561a@ensfsi.onmicrosoft.com>
2025-11-17 17:05:46 +0100

    création de la classe Commercant

--- src/histoire/Commercant.java
+++ src/histoire/Commercant.java
@@ -1,5 +1,17 @@
 package histoire;
 
-public class Commercant {
-
+public class Commercant extends Humain{
+	public Commercant(String nom, int argent) {
+		super(nom, "the" , argent);
+	}
+	
+	
+	public int seFaireExtorquer() {
+		System.out.println(parler()+ "J'ai tout perdu! Le monde est trop injuste...");
+		return getArgent();
+	}
+	
+	public void recevoir(int argent) {
+		System.out.println(parler() + argent + " sous! Je te remercie genereux donateur!");
+	}
 }
