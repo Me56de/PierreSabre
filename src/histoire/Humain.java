9aba1df057298d55e8f3bdbb7cbb6ab41e3d4029
Mederic Mognat <mgm5561a@ensfsi.onmicrosoft.com>
2025-11-17 16:48:15 +0100

    création de la classe Humain

---
+++ src/histoire/Humain.java
@@ -0,0 +1,52 @@
+package histoire;
+
+public class Humain {
+	private String nom;
+	private String boisson;
+	private int argent;
+	
+	
+	public Humain(String nom, String boisson, int argent) {
+		this.nom = nom;
+		this.boisson = boisson;
+		this.argent = argent;
+	}
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	
+	public String parler() {
+		return "(" + nom + ")" + "-";
+	}
+	public void direBonjour() {
+		System.out.println(parler() + "Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boisson + ".");
+	}
+	
+	public void boire() {
+		System.out.println(parler() + "Mmmm, un bon verre de " + boisson + " ! GLOUPS !");
+	}
+	
+	public void acheter(String bien,int prix) {
+		if (prix < argent) {
+			System.out.println(parler() + "J'ai " + argent + " sous en poche. Je vais pouvoir m'offrir " + bien  + " à " + prix + " sous. ");
+			perdreArgent(prix);
+		}
+		else {
+			System.out.println(parler() + "Je n'ai plus que " + argent + " sous en poche. Je ne peux même pas m'offrir " + bien  + " à " + prix + " sous. ");
+		}
+	}
+	
+	private int gagnerArgent(int gain) {
+		return argent = argent + gain ;
+	}
+	
+	private int perdreArgent(int perte) {
+		return argent = argent - perte;
+	}
+	
+}
